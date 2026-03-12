4c491dfd010d2a36d9c0729c3ecf16a0411245c2
c8763yee <rrrrr4413@gmail.com>
2026-03-12 17:48:56 +0800

    feat: add list_add macro
    Signed-off-by: c8763yee <rrrrr4413@gmail.com>

--- include/list.h
+++ include/list.h
@@ -2,11 +2,20 @@ struct list_head {
 	struct list_head *prev, *next;
 };
 
-typedef struct {
-	int value;
-	struct list_head lst;
+typedef struct value {
+	int val;
+	struct list_head list;
 } value_t;
 
 #define LIST_HEAD(name) { &name, &name }
+#define LIST_HEAD_INIT(name) struct list_head name = LIST_HEAD(name)
 
-#define INIT_LIST_HEAD(name) struct list_head name = LIST_HEAD(name);
+static inline void list_add(struct list_head *node, struct list_head *head)
+{
+	struct list_head *next = head->next;
+	node->next = next;
+	node->prev = head;
+	head->next = node;
+	next->prev = node;
+}
+
